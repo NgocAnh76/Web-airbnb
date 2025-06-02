439b3a15f85643c141c14ad0c7c2d735a4c01553
NgocAnh76 <ngocanh76.dev@gmail.com>
2025-06-02 16:11:51 +0700

    update

--- src/components/constant/app.constant.ts
+++ src/components/constant/app.constant.ts
@@ -1,7 +1,7 @@
 // export let isProduction = import.meta.env.VITE_IS_PRODUCTION === "true";
 
 export const BASE_DOMAIN_API =
-  process.env.NEXT_PUBLIC_BASE_DOMAIN_API || `http://45.76.191.187:3070/`;
+  process.env.NEXT_PUBLIC_API_URL || `http://45.76.191.187:3070/`;
 export const ACCESS_TOKEN = `ACCESS_TOKEN`;
 export const REFRESH_TOKEN = `REFRESH_TOKEN`;
 export const USER = `USER`;
